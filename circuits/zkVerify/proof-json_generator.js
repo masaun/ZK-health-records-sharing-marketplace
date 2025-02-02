29bf8fc271f0d7acdcd5338b20362bab29e56e5e
masaun <masaun@protonmail.com>
2025-02-02 11:18:33 +0900

    [circuit - sh]: Add an array format ([]) to the 'ultraplonk_zkv_json_to_be_generated'

--- circuits/zkVerify/proof-json_generator.js
+++ circuits/zkVerify/proof-json_generator.js
@@ -1,14 +1,19 @@
+const fs = require("fs");
+
 const proof = require("./output_zkverify_version_of_proof_pubs_vk/zkv_health_data_sharing_proof.hex");
 const pubs = require("./output_zkverify_version_of_proof_pubs_vk/zkv_health_data_sharing_pubs.hex");
 const vk = require("./output_zkverify_version_of_proof_pubs_vk/zkv_health_data_sharing_vk.hex");
-
-const fs = require("fs");
+console.log("proof:", JSON.stringify(proof));
+console.log("pubs:", JSON.stringify(pubs));
+console.log("vk:", JSON.stringify(vk));
 
 const ultraplonk_zkv_json_to_be_generated =
 {
-    "vk": vk,
-    "publicSignals": pubs,
-    "proof": proof
+    "vk": JSON.stringify(vk.toString()),
+    "publicSignals": [
+        JSON.stringify(pubs.toString())
+    ],
+    "proof": JSON.stringify(proof.toString())
 };
 
 const jsonData = JSON.stringify(ultraplonk_zkv_json_to_be_generated, null, 2);
