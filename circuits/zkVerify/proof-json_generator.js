713a0a86cce76c1fbfe1075d97e3a4415d2ea216
masaun <masaun@protonmail.com>
2025-02-02 10:08:21 +0900

    [sh]: Add the proof-json_generator.js to the convert_ultraplonk_to_zkverify_proof.sh

---
+++ circuits/zkVerify/proof-json_generator.js
@@ -0,0 +1,22 @@
+const proof = require("zkv_health_data_sharing_proof.hex");
+const pubs = require("zkv_health_data_sharing_pubs.hex");
+const vk = require("zkv_health_data_sharing_vk.hex");
+
+const fs = require("fs");
+
+const ultraplonk_zkv_json_to_be_generated =
+{
+    "vk": vk,
+    "publicSignals": pubs,
+    "proof": proof
+};
+
+const jsonData = JSON.stringify(ultraplonk_zkv_json_to_be_generated, null, 2);
+
+fs.writeFile("./final-output/ultraplonk_health_data_sharing_zkv.json", jsonData, 'utf8', (err) => {
+    if (err) {
+        console.error('Error writing to file', err);
+    } else {
+        console.log('Data written to file');
+    }
+});
--- circuits/zkVerify/ultraplonk_verifier/convert_ultraplonk_to_zkverify_proof.sh
+++ circuits/zkVerify/ultraplonk_verifier/convert_ultraplonk_to_zkverify_proof.sh
@@ -11,6 +11,9 @@ echo "Convert a VK of the original Ultraplonk proof to a zkVerify verion of VK"
 noir-cli key --input ../../target/health_data_sharing_vk.bin --output ../output_zkverify_version_of_proof_pubs_vk/zkv_health_data_sharing_vk.bin
 
 echo "Verify the converted-Proof, Pubs, VK"
-noir-cli verify --key  ../output_zkverify_version_of_proof_pubs_vk/zkv_health_data_sharing_vk.bin --proof ../output_zkverify_version_of_proof_pubs_vk/zkv_health_data_sharing_proof.bin --pubs ../output_zkverify_version_of_proof_pubs_vk/zkv_health_data_sharing_pubs.bin
+noir-cli verify --key ../output_zkverify_version_of_proof_pubs_vk/zkv_health_data_sharing_vk.bin --proof ../output_zkverify_version_of_proof_pubs_vk/zkv_health_data_sharing_proof.bin --pubs ../output_zkverify_version_of_proof_pubs_vk/zkv_health_data_sharing_pubs.bin
+
+echo "Generate a JSON file includes the converted-Proof, Pubs, VK in the /final_output directory"
+node ../zkVerify/proof-json_generator.js
 
 echo "Done"
