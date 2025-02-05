ef87e701edfbbb802c9d2f58405114a072793d2f
masaun <masaun@protonmail.com>
2025-02-06 08:44:48 +0900

    [SC]: Add the 'AttestationPosted' event to the IZkVerifyAttestation.sol

--- contracts/zkv-attestation-contracts/interfaces/IZkVerifyAttestation.sol
+++ contracts/zkv-attestation-contracts/interfaces/IZkVerifyAttestation.sol
@@ -7,6 +7,12 @@ pragma solidity ^0.8.20;
  */
 interface IZkVerifyAttestation {
 
+    /// @notice Emitted when a new attestation is posted.
+    /// @notice Reference from the deployed-contract on EDU Chain: https://edu-chain-testnet.blockscout.com/address/0x147AD899D1773f5De5e064C33088b58c7acb7acf?tab=contract
+    /// @param _attestationId Event attestationId.
+    /// @param _proofsAttestation Aggregated proofs attestation.
+    event AttestationPosted(uint256 indexed _attestationId, bytes32 indexed _proofsAttestation);
+
     function submitAttestation(
         uint256 _attestationId,
         bytes32 _proofsAttestation) external;
