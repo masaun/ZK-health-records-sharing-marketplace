3abb2d0eb0997a08c41fd194210a5623c058e658
masaun <masaun@protonmail.com>
2025-02-09 11:07:40 +0900

    [Frontend /w NoirJS]: Commentout the getCircuit() - because it cause the error (= 'Module parse failed: Unexpected token')

--- app/src/hooks/proverWithNoirJS.ts
+++ app/src/hooks/proverWithNoirJS.ts
@@ -1,7 +1,7 @@
 import { useState, useEffect, useRef } from 'react';
 import { useAccount } from '@/context/AccountContext';
 
-import { compile, createFileManager } from "@noir-lang/noir_wasm"
+import { compile, createFileManager } from "@noir-lang/noir_wasm";
 import main from "../../../circuits/src/main.nr";
 import nargoToml from "../../../circuits/Nargo.toml";
 
@@ -29,12 +29,15 @@ export function proverWithNoirJS() {
 /**
  * @notice - Get the circuit from the ZK circuit in Noir
  */
-export async function getCircuit() {
-    const fm = createFileManager("/");
-    const { body } = await fetch(main);
-    const { body: nargoTomlBody } = await fetch(nargoToml);
+/// [Error]: The following getCircuit() cause the error below: 
+/// Module parse failed: Unexpected token (2:4)
+/// You may need an appropriate loader to handle this file type, currently no loaders are configured to process this file. See https://webpack.js.org/concepts#loaders
+// export async function getCircuit() {
+//     const fm = createFileManager("/");
+//     const { body } = await fetch(main);
+//     const { body: nargoTomlBody } = await fetch(nargoToml);
    
-    fm.writeFile("../../../circuits/src/main.nr", body);
-    fm.writeFile("../../../circuits/Nargo.toml", nargoTomlBody);
-    return await compile(fm);
-}
+//     fm.writeFile("../../../circuits/src/main.nr", body);
+//     fm.writeFile("../../../circuits/Nargo.toml", nargoTomlBody);
+//     return await compile(fm);
+// }
