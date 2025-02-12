5add5aa32074c50c8ed2593e639b0d4e0d060110
masaun <masaun@protonmail.com>
2025-02-13 08:30:36 +0900

    [FE]: Remove 2 args ('medicalResearcherId', 'healthDataSharingRequestId') from ABI of the HealthDataSharingExecutor#submitHealthData()

--- app/src/hooks/useZkVerify.ts
+++ app/src/hooks/useZkVerify.ts
@@ -174,7 +174,7 @@ export function useZkVerify() {
 
             /// @dev - HealthDataSharingExecutor#submitHealthData()
             const abiAppContract = [
-                "function submitHealthData(bytes calldata proof, bytes32[] calldata publicInput, uint256 medicalResearcherId, uint256 healthDataSharingRequestId, uint256 _attestationId, bytes32 _leaf, bytes32[] calldata _merklePath, uint256 _leafCount, uint256 _index)"
+                "function submitHealthData(bytes calldata proof, bytes32[] calldata publicInput, uint256 _attestationId, bytes32 _leaf, bytes32[] calldata _merklePath, uint256 _leafCount, uint256 _index)"
             ];
 
             const zkvContract = new Contract(process.env.NEXT_PUBLIC_EDU_CHAIN_ZKVERIFY_CONTRACT_ADDRESS, abiZkvContract, provider);
